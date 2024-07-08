5be53e50f8dd67c022b6395d5b81a1beec385887
iameva01 <evatagirova@gmail.com>
2024-07-09 01:12:52 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,10 @@
+name = 'Eva'
+print(name)
+age = 23
+print(age)
+age = 23 + 18
+print(age)
+is_student = True
+print(is_student)
+
+print('Name: ', name, ',', 'Age: ', (age-18), ',', 'New age: ', age, ',', 'Is student: ', is_student) #эта часть была необязательна для выполнения, но было интересно переписать это в таком виде
